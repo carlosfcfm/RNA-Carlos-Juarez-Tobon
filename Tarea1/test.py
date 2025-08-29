e95ca44c5286ca7ac0a093d087a14dfedd933d85
carlosfcfm <carlos.juarez.ihc@gmail.com>
2025-08-28 21:26:12 -0600

    Implementé la documentación de newtwork.py y test.py comentando ambos códigos

--- Tarea1/test.py
+++ Tarea1/test.py
@@ -1,16 +1,24 @@
-import mnist_loader
+import mnist_loader # Cargamos el dataset del MNIST para entrenar a la red
 import network as network
-import pickle
+import pickle #Esto será importante para guardar archivos
 
-training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
+
+# Aquí cargamos los datos para entrenar, para ajustar hiperparámetros y para evaluar el rendimiento
+training_data, validation_data , test_data = mnist_loader.load_data_wrapper() 
 
 training_data = list(training_data)
 test_data = list(test_data)
 
+# Definimos las características de la red, 
+# 784 neuronas de entrada (input)
+# 30 neuronas en la capa media
+# 10 neuronas en la capa de salida (output)
 net=network.Network([784,30,10])
 
+# Aquí definimos 15 épocas, con un mini_batch de tamaño 10 y un learning rate de 0.9
 net.SGD( training_data, 15, 10, 0.9, test_data=test_data)
 
+# Aquí abrimos el archivo ".pkl" generado al finalizar el entrenamiento y lo guardamos
 archivo = open("red_prueba.pkl",'wb') 
 pickle.dump(net,archivo) 
 archivo.close() 
