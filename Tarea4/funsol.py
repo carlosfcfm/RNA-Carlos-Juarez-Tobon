003675589883ec0f0dd67e35f40c8ac731662e32
carlosfcfm <carlos.juarez.ihc@gmail.com>
2025-09-29 00:44:25 -0600

    Este es el código para la parte 2 de la tarea 4, con conectividad a DagsHub y mlflow. En este intento, reproduzco a la función 3*sin(pi*x) en el intervalo [-1,1]

--- Tarea4/funsol.py
+++ Tarea4/funsol.py
@@ -1,12 +1,22 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD, RMSprop, Adam
 from matplotlib import pyplot as plt
 import numpy as np
-import math
+import mlflow
+import os
+from getpass import getpass
 
+############### Inicializamos conectividad con el repositorio "Experimentos" en DagsHub ###########
+REPO_NAME= "Experimentos"
+REPO_OWNER= "carlosfcfm"  #Escribir nombre de repositorio
+USER_NAME = "carlosfcfm" #Escribir su usuario
+os.environ['MLFLOW_TRACKING_USERNAME'] = USER_NAME
+os.environ['MLFLOW_TRACKING_PASSWORD'] = getpass('Enter your DAGsHub access token or password: ')
+mlflow.set_tracking_uri(f'https://dagshub.com/{REPO_OWNER}/{REPO_NAME}.mlflow')
+###################################################################################################
+
+######## Construcción del modelo y de la capa personalizada ####################
 loss_tracker = keras.metrics.Mean(name="loss")
 
 class Funsol(keras.Model):
@@ -18,6 +28,7 @@ class Funsol(keras.Model):
         batch_size = 10
         x = tf.random.uniform((batch_size,), minval=-1, maxval=1)
         eq = 3 * tf.math.sin(np.pi* x)
+
 
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)
@@ -51,26 +62,38 @@ class SinTransform(tf.keras.layers.Layer):
         modes = tf.math.sin(args)
         return tf.tensordot(modes, self.kernel, 1)
     
-trans = SinTransform(5)
-x = tf.random.uniform((3,), minval=-1, maxval=1)
-print("Input:", x)
-res = trans(x)
-print("Output:", res)
 
 inputs = keras.Input(shape=(1,))
 print(inputs)
 x = SinTransform(5)(inputs)
 model = Funsol(inputs=inputs,outputs=x)
 model.summary()
+###################################################################
 
-model.compile(optimizer=Adam(learning_rate=0.01), metrics=['loss'])
+#### Setup y entrenamiento del modelo con logging de MLflow #########################
+filepath = "best_model_funsol1.keras"
+mlflow.set_experiment("Funsol")
+
+mlflow.start_run(nested=True)
+mlflow.tensorflow.autolog(log_models=False)
+###################################################################################
+
+
+########## Compilación del modelo y entrenamiento ##########################
+model.compile(optimizer=SGD(learning_rate=0.01), metrics=['loss'])
 x=tf.linspace(-1,1,100)
-history = model.fit(x,epochs=50,verbose=1)
+history = model.fit(x,epochs=30,verbose=1)
+###########################################################################
+
+######### Salvamos el modelo y lo loggeamos #########
+model.save(filepath)
+mlflow.log_artifact(filepath, artifact_path="models")
+mlflow.end_run()
+######################################################
 
 x_testv = tf.linspace(-1,1,100)
 a=model.predict(x_testv)
 
 plt.plot(x_testv,a)
-plt.plot(x_testv, 3 * tf.math.sin(np.pi* x_testv))
+plt.plot(x_testv, 3 * tf.math.sin(np.pi* x))
 plt.show()
-
