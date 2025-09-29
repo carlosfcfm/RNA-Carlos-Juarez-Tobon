e25fcc6f54cf8b7a2772ace3cc55122e3a26822e
carlosfcfm <carlos.juarez.ihc@gmail.com>
2025-09-29 16:46:48 -0600

    Cambié la arquitectura de la red, ya que estaba haciendo una red con una sola capa y en la tarea solo pide una red que reproduzca las funciones establecidas en el punto 2, esta red, es mucho más completa y no hace uso de una capa personalizada

--- Tarea4/funsol.py
+++ Tarea4/funsol.py
@@ -16,7 +16,7 @@ os.environ['MLFLOW_TRACKING_PASSWORD'] = getpass('Enter your DAGsHub access toke
 mlflow.set_tracking_uri(f'https://dagshub.com/{REPO_OWNER}/{REPO_NAME}.mlflow')
 ###################################################################################################
 
-######## Construcción del modelo y de la capa personalizada ####################
+######## Construcción del modelo ####################
 loss_tracker = keras.metrics.Mean(name="loss")
 
 class Funsol(keras.Model):
@@ -25,10 +25,9 @@ class Funsol(keras.Model):
         return [loss_tracker]
     
     def train_step(self, data):
-        batch_size = 10
+        batch_size = 300
         x = tf.random.uniform((batch_size,), minval=-1, maxval=1)
-        eq = 1 + 2*x + 4*tf.math.pow(x, 3)
-
+        eq = 3 * tf.math.sin(np.pi * x)
 
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)
@@ -41,32 +40,11 @@ class Funsol(keras.Model):
 
         return {"loss": loss_tracker.result()}
 
-class SinTransform(tf.keras.layers.Layer):
-    def __init__(self, num_outputs):
-        super(SinTransform, self).__init__()
-        self.num_outputs = int(num_outputs)
-
-        self.freq = tf.range(1, self.num_outputs + 1, dtype=tf.float32)
-
-        self.kernel = self.add_weight(shape=[self.num_outputs])
-
-    def call(self, inputs):
-        inputs = tf.convert_to_tensor(inputs)
-        if (inputs.shape == ()):
-           inputs=(inputs,)
-        elif (len(inputs.shape) == 1):
-            inputs = tf.expand_dims(inputs, axis=1)
-        batch = tf.shape(inputs)[0]
-        freq_b = tf.ones([batch, 1]) * self.freq
-        args = freq_b * inputs
-        modes = tf.math.sin(args)
-        return tf.tensordot(modes, self.kernel, 1)
-    
-
 inputs = keras.Input(shape=(1,))
-print(inputs)
-x = SinTransform(4)(inputs)
-model = Funsol(inputs=inputs,outputs=x)
+x = keras.layers.Dense(150, activation='tanh')(inputs)
+x = keras.layers.Dense(100, activation='tanh')(x)
+x = keras.layers.Dense(1)(x)
+model = Funsol(inputs=inputs, outputs=x)
 model.summary()
 ###################################################################
 
@@ -80,9 +58,9 @@ mlflow.tensorflow.autolog(log_models=False)
 
 
 ########## Compilación del modelo y entrenamiento ##########################
-model.compile(optimizer=SGD(learning_rate=0.05), metrics=['loss'])
+model.compile(optimizer=Adam(learning_rate=0.05), metrics=['loss'])
 x=tf.linspace(-1,1,100)
-history = model.fit(x,epochs=30,verbose=1)
+history = model.fit(x,epochs=70,verbose=1)
 ###########################################################################
 
 ######### Salvamos el modelo y lo loggeamos #########
@@ -95,5 +73,6 @@ x_testv = tf.linspace(-1,1,100)
 a=model.predict(x_testv)
 
 plt.plot(x_testv,a)
-plt.plot(x_testv, 1 + 2 * x_testv + 4 * tf.math.pow(x_testv, 3))
+plt.plot(x_testv, 3 * tf.math.sin(np.pi * x))
 plt.show()
+
