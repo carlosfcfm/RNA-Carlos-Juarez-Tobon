b8892960568e5ebaa9e01dfab79239c2889729ff
carlosfcfm <carlos.juarez.ihc@gmail.com>
2025-09-29 21:56:39 -0600

    Ahora se modifica el código para que se puedan aproximar ecuaciones diferenciales de segundo orden

--- Tarea4/ecdif.py
+++ Tarea4/ecdif.py
@@ -23,16 +23,27 @@ class ODEsolver(Sequential):
          max = tf.cast(tf.reduce_max(data),tf.float32)
          x = tf.random.uniform((batch_size,1), minval=min, maxval=max)
 
-         with tf.GradientTape() as tape:
+         with tf.GradientTape(persistent=True) as tape:
+             tape.watch(x)  # Esta línea es para obtener 2ª derivada respecto a x y evitar errores
              with tf.GradientTape() as tape2:
                  tape2.watch(x)
                  y_pred = self(x, training=True)
              dy = tape2.gradient(y_pred, x) #derivada del modelo con respecto a entradas x
+             ydd = tape.gradient(dy, x) #derivada segunda del modelo con respecto a entradas x
              x_o = tf.zeros((batch_size,1)) #valor de x en condicion inicial x_0=0
+
+             # Evaluamos y(x0) y y'(x0) para imponer condiciones iniciales en la ec.dif de segundo orden
+             # usamos un tape separado para obtener la derivada en x_o
+             with tf.GradientTape() as tape_ic:
+                 tape_ic.watch(x_o)
+                 y_o = self(x_o, training=True)
+             dy_o = tape_ic.gradient(y_o, x_o)
+
              y_o = self(x_o,training=True) #valor del modelo en en x_0
-             eq = x*dy+y_pred-(x**2)*tf.math.cos(x) #Ecuacion diferencial evaluada en el modelo. Queremos que sea muy pequeno
-             ic = 0. #valor que queremos para la condicion inicial o el modelo en x_0
-             loss = self.mse(0., eq) + self.mse(y_o,ic)
+             eq = ydd + y_pred #Ecuacion diferencial evaluada en el modelo. Queremos que sea muy pequeno
+             ic = 1. #valor que queremos para la condicion inicial o el modelo en x_0
+             # condiciones iniciales: y(0)=1, y'(0)=-0.5
+             loss = self.mse(0., eq) + self.mse(y_o,ic) + self.mse(dy_o, -0.5)
 
         # Apply grads
          grads = tape.gradient(loss, self.trainable_variables)
@@ -44,8 +55,8 @@ class ODEsolver(Sequential):
 
 model = ODEsolver()
 
-model.add(Dense(30, activation='tanh', input_shape=(1,)))
-model.add(Dense(30, activation='tanh'))
+model.add(Dense(50, activation='tanh', input_shape=(1,)))
+model.add(Dense(50, activation='tanh'))
 model.add(Dense(1))
 
 
@@ -60,7 +71,7 @@ history = model.fit(x,epochs=400,verbose=1)
 x_testv = tf.linspace(-5,5,100)
 a=model.predict(x_testv)
 plt.plot(x_testv,a,label="aprox")
-plt.plot(x_testv,(((x**2)-2)*np.sin(x)*(1/x)+(2*np.cos(x))),label="exact")
+plt.plot(x_testv,(np.cos(x_testv) - 0.5*np.sin(x_testv)),label="exact")
 plt.legend()
 plt.show()
 
